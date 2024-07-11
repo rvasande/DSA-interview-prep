2ed72202ac61c3f2c5e072f4918bce596edeaa20
rushikesh <rushikeshvasande333@gmail.com>
2024-07-12 00:14:06 +0530

    2 problem solved

---
+++ daily-solved-problems/problems.js
@@ -0,0 +1,43 @@
+//1) reverse string 
+function reverseStr(str){
+    let revrs = str.split("")
+    let result = revrs.reverse().join("")
+
+    if(str === result){
+        console.log(`string is pallindrom and reverse string is ===> ${result}`);
+
+        }else{
+            console.log(`string is not pallindrom and reverse string is  ===> ${result}`);
+
+        }
+        
+}
+// reverseStr('olo')
+
+// another approach 
+function anotherAppr(n){
+    let str = ''
+    for(let i=n.length-1; i>=0; i--){
+        str = str.concat(n[i])
+        }
+    console.log(str)
+}
+// anotherAppr('vassu')
+
+
+// 2) find largest number from array
+{
+    let arr = [1,2,3,4,5,64,9]
+
+    function largestNo(arr){
+        let largestNumber = arr[0]
+
+        arr.forEach(element => {
+            if(element > largestNumber){
+                largestNumber = element
+            }
+        });
+        console.log(largestNumber);
+    }
+    largestNo(arr)
+}
